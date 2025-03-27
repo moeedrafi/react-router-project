684f9380a7b3dba2aeefd32397466a8dba29d1a7
Moeed Rafi <moeedrafi95@gmail.com>
2025-03-27 17:59:37 +0500

    feat: Players Page Data

--- src/components/Players.tsx
+++ src/components/Players.tsx
@@ -1,8 +1,61 @@
-import { useSearchParams } from "react-router";
+import { Link, useLocation, useSearchParams } from "react-router";
+import { Teams } from "../utils/types";
+import usePlayerNames from "../hooks/usePlayerNames";
+import { slugify } from "../utils";
+
+function CustomLink({
+  children,
+  to,
+}: {
+  children: React.ReactNode;
+  to: string;
+}) {
+  const location = useLocation();
+  const playerId = location.pathname.split("/")[2];
+  const match = playerId === to;
+
+  const styles = match ? { fontWeight: 900, color: "white" } : {};
+
+  return (
+    <li>
+      <Link
+        style={{ ...styles }}
+        to={{ pathname: to, search: location.search }}
+      >
+        {children}
+      </Link>
+    </li>
+  );
+}
+
+function Sidebar({ title, list }: { title: string; list: string[] }) {
+  return (
+    <div>
+      <h3 className="header">{title}</h3>
+      <ul className="sidebar-list">
+        {list.map((item) => (
+          <CustomLink to={`${slugify(item)}`} key={item}>
+            {item.toUpperCase()}
+          </CustomLink>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 export const Players = () => {
   const [searchParams] = useSearchParams();
-  const teamId = searchParams.get("teamId");
+  const team = searchParams.get("teamId");
+  const { loading, response } = usePlayerNames(team as Teams);
+  const names = response as string[];
 
-  return <div className="container">Players for {teamId}</div>;
+  if (loading) {
+    return <p>LOADING...</p>;
+  }
+
+  return (
+    <div className="container">
+      <Sidebar title="Players" list={names} />
+    </div>
+  );
 };
--- src/hooks/usePlayerNames.ts
+++ src/hooks/usePlayerNames.ts
@@ -1,6 +1,9 @@
-import { Player } from "../utils/types";
+import { Teams } from "../utils/types";
 import useFetch from "./useFetch";
 
-export default function usePlayerNames(team: Player) {
+export default function usePlayerNames(team: Teams): {
+  loading: boolean;
+  response: string[] | null;
+} {
   return useFetch("/players?filter=names", "POST", JSON.stringify({ team }));
 }
